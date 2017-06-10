67f13e83542a01feb48d676701d226a08562658e
David Catuhe <david.catuhe@live.fr>
2017-06-10 09:09:44 -0700

    Revert "Car Mode #2 added"

--- Podcasts/Pages/Shell.xaml.cs
+++ Podcasts/Pages/Shell.xaml.cs
@@ -858,15 +858,5 @@ namespace Podcasts
             RemainingDuration.Visibility = Visibility.Collapsed;
             LocalSettings.Instance.FavorRemainingDuration = false;
         }
-
-        private void Car_Click(object sender, RoutedEventArgs e)
-        {
-            VisualStateManager.GoToState(this, "Car", true);
-        }
-
-        private void Street_Click(object sender, RoutedEventArgs e)
-        {
-            VisualStateManager.GoToState(this, "Medium", true);
-        }
     }
 }
